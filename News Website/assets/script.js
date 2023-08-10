61f8970b2776d0958df3c61860cc48285cebdb1f
Pratham1607 <78417410+Pratham1607@users.noreply.github.com>
2023-08-10 18:54:34 +0530

    script.js

--- News Website/assets/script.js
+++ News Website/assets/script.js
@@ -8,36 +8,24 @@ function reload() {
     window.location.reload();
 }
 
-// JavaScript function to toggle mobile menu
-function toggleMobileMenu() {
-    const mobileMenu = document.querySelector('.mobile-menu');
-    mobileMenu.classList.toggle('show');
-}
-
 async function fetchNews(query) {
     const res = await fetch(`${url}${query}&apiKey=${API_KEY}`);
     const data = await res.json();
-    // console.log(data);
     bindData(data.articles);
 }
 
 function bindData(articles) {
-    // Making clones of card and appending data in it
     const cardsContainer = document.getElementById('cards-container');
     const newsCardTemplate = document.getElementById('template-news-card');
 
     cardsContainer.innerHTML = '';
 
     articles.forEach((article) => {
-        // If an article does not have any image skip the news
         if (!article.urlToImage) return;
-
-        // To clone everything inside of template-news-card id
         const cardClone = newsCardTemplate.content.cloneNode(true);
 
         fillDataInCard(cardClone, article);
-
-        // Now appending the clones into cardContainer
+        
         cardsContainer.appendChild(cardClone);
     });
 }
@@ -49,7 +37,6 @@ function fillDataInCard(cardClone, article) {
     const newsSource = cardClone.querySelector('#news-source');
     const newsDesc = cardClone.querySelector('#news-desc');
 
-    // getting details from the API
     newsImg.src = article.urlToImage;
     newsTitle.innerHTML = article.title;
     newsDesc.innerHTML = article.description;
@@ -66,7 +53,6 @@ function fillDataInCard(cardClone, article) {
 
 let curSelectedNav = null;
 function onNavItemClick(id) {
-    // Will fetch the use and bind the data
     fetchNews(id);
     const navItem = document.getElementById(id);
     curSelectedNav?.classList.remove("active");
@@ -74,11 +60,9 @@ function onNavItemClick(id) {
     curSelectedNav.classList.add("active");
 }
 
-// Searching
 const searchButton = document.getElementById("search-button");
 const searchText = document.getElementById("search-text");
 
-// Function to perform the search
 function performSearch() {
     const query = searchText.value;
     if (!query) return;
@@ -87,23 +71,10 @@ function performSearch() {
     curSelectedNav = null;
 }
 
-// clicking on the button
-// searchButton.addEventListener("click", () => {
-//     const query = searchText.value;
-//     if (!query) return;
-//     fetchNews(query);
-//     curSelectedNav?.classList.remove("active");
-//     curSelectedNav = null;
-// });
-
-// clicking on the button
 searchButton.addEventListener("click", () => {
     performSearch();
 });
 
-
-// Adding the enter functionality
-// Add event listener for Enter key press in the input field
 searchText.addEventListener("keydown", (event) => {
     if (event.key === "Enter") {
         performSearch();
